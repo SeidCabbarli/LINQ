16a0b0745da12cfd83ee3e6fa8e14e39f436837c
SeidCabbarli <seidcabbarli13@gmail.com>
2025-04-10 13:11:46 +0400

    Add files via upload

---
+++ LINQ.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+class Program
+{
+    static void Main()
+    {
+        List<int> regemler = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        var tekregemler = regemler.Where(reg => reg % 2 != 0).ToList();
+
+        Console.WriteLine("Tek regemler: ");
+        foreach( var reg in tekregemler)
+        {
+            Console.WriteLine(reg);
+        }
+    }
+}
---
+++ LINQ2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    internal class LINQ2
+    {
+        static void Main()
+        {
+            List<string> adlist = new List<string> { "seid", "ali", "nigar", "fidan" };
+
+            var uzunluq = adlist.Select(ad => ad.Length).ToList();
+
+            Console.WriteLine("adlarin uzunluqlari: ");
+            foreach (var ad in uzunluq)
+            {
+                Console.WriteLine(ad);
+            }
+        }
+    }
+}
